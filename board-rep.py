92c7f5eebf30d1bbde64f2ea24d5b3d118fe1a2f
mohamed <m_omar01@protonmail.com>
2020-01-10 12:14:51 +0000

    created mailbox board array

--- board-rep.py
+++ board-rep.py
@@ -1,13 +1,29 @@
-board = [[0 for x in range(12)] for x in range(12)]
-boundary = (0, 1, 10, 11)
+board = [0 for x in range(120)]
+illegal = 99
 
-for i, row in enumerate(board):
-    if i in boundary:
-        board[i] = ["-" for n in range(12)]
+for x in range(20):
+    board[x] = illegal
+
+for y in range(100, 120):
+    board[y] = illegal
+
+n = 20
+while n < 100:
+    board[n] = illegal
+    board[n + 9] = illegal
+    n += 10
+
+#  4 buffer rows and 2 buffer columns made to avoid off-board moves
+
+n = 1
+m = 0
+
+while n <= 64:
+    if board[m] == illegal:
+        pass
     else:
-        for point in boundary:
-            row[point] = "-"
+        board[m] = n
+        n += 1
+    m += 1
 
-for x in board:
-    print(x)
-
+#  squares on the board are numbered from 1-64
