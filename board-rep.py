50625a0fa8178dbc9e7b5fc7dfda24e770a75608
mohamed <m_omar01@protonmail.com>
2020-01-22 10:21:48 +0000

    Created a "Piece" class and defined the instances of this class for each piece type/colour.

--- board-rep.py
+++ board-rep.py
@@ -1,5 +1,6 @@
 board = [0 for x in range(120)]
 illegal = 88
+fifty_move_counter = 0
 
 for x in range(20):
     board[x] = illegal
@@ -15,37 +16,44 @@ while n < 100:
 
 # 4 buffer rows and 2 buffer columns made to avoid off-board moves
 
-pieces = {
-    "pa": 1,
-    "bi": 3,
-    "kn": 3,
-    "ro": 5,
-    "qu": 9,
-    "ki": 100000000
-}  # piece values
+letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
+digits = list(range(1, 9))
+
+ranks = dict(zip(letters, digits))
+
+digit_strings = [str(x) for x in digits]
+tens = [20, 30, 40, 50, 60, 70, 80, 90]
+
+files = dict(zip(digit_strings, tens))
+
+# encodes standard chess position references (e.g. "e1") to the array index
 
 U = 10
 D = -10
 R = 1
 L = -1
 
-moves = {
-    "pa": (U, U + U, U + R, U + L ),
-    "bi": (U + R, U + L, D + R, D + L),
-    "kn": (U + U + R, U + U + L, D + D + R, D + D + L),
-    "ro": (U, D, R, L),
-    "qu": (U, D, R, L, U + R, U + L, D + R, D + L),
-    "ki": (U, D, R, L, U + R, U + L, D + R, D + L)
-}
+# directions to move
 
-# possible moves for all pieces
 
-letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
-digits = list(range(1, 9))
-ranks = dict(zip(letters, digits))
+class Piece:
+    def __init__(self, piece_type, colour, moves, value):
+        self.piece_type = piece_type
+        self.colour = colour
+        self.moves = moves
+        self.value = value
 
-tens = [20, 30, 40, 50, 60, 70, 80, 90]
-digit_strings = [str(x) for x in digits]
-files = dict(zip(tens, digit_strings))
 
-# encodes standard chess position references (e.g. "e1") to the array index
+white_pawn = Piece(1, "white", (U, U + U, U + R, U + L ), 1)
+white_bishop = Piece(2, "white", (U + R, U + L, D + R, D + L), 3)
+white_knight = Piece(3, "white", (U + U + R, U + U + L, D + D + R, D + D + L), 3)
+white_rook = Piece(4, "white", (U, D, R, L), 5)
+white_queen = Piece(5, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 9)
+white_king = Piece(6, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000)
+
+black_pawn = Piece(1, "black", (U, U + U, U + R, U + L ), 1)
+black_bishop = Piece(2, "black", (U + R, U + L, D + R, D + L), 3)
+black_knight = Piece(3, "black", (U + U + R, U + U + L, D + D + R, D + D + L), 3)
+black_rook = Piece(4, "black", (U, D, R, L), 5)
+black_queen = Piece(5, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 9)
+black_king = Piece(6, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000)
