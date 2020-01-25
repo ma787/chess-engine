fc53b4b5703962a1108858612e649416cfd68ac0
mohamed <m_omar01@protonmail.com>
2020-01-25 19:42:49 +0000

    Set the initial positions of each piece on the board by marking them as occupied. Adjusted the instances of the piece class and added a variable to track their positions on the board.

--- board-rep.py
+++ board-rep.py
@@ -1,6 +1,7 @@
 board = [0 for x in range(120)]
 illegal = 88
 fifty_move_counter = 0
+side_to_move = "white"
 
 for x in range(20):
     board[x] = illegal
@@ -35,25 +36,51 @@ L = -1
 
 # directions to move
 
+initial_positions = {
+    "wp": [x for x in range(31, 39)],
+    "wb": [23, 26],
+    "wn": [22, 27],
+    "wr": [21, 28],
+    "wq": [24],
+    "wk": [25],
+    "bp": [x for x in range(81, 89)],
+    "bb": [93, 96],
+    "bn": [92, 97],
+    "br": [91, 98],
+    "bq": [94],
+    "bk": [95],
+}
+
+# the positions in the array of the pieces in the initial chess arrangement
+
+for row in initial_positions.values():
+    for item in row:
+        board[item] = 1
+
+# setting the initial board positions
+
 
 class Piece:
-    def __init__(self, piece_type, colour, moves, value):
+    def __init__(self, piece_type, colour, moves, value, positions):
         self.piece_type = piece_type
         self.colour = colour
         self.moves = moves
         self.value = value
+        self.positions = positions
 
 
-white_pawn = Piece(1, "white", (U, U + U, U + R, U + L ), 1)
-white_bishop = Piece(2, "white", (U + R, U + L, D + R, D + L), 3)
-white_knight = Piece(3, "white", (U + U + R, U + U + L, D + D + R, D + D + L), 3)
-white_rook = Piece(4, "white", (U, D, R, L), 5)
-white_queen = Piece(5, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 9)
-white_king = Piece(6, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000)
+white_pawn = Piece(1, "white", (U, U + U, U + R, U + L), 1, initial_positions["wp"])
+white_bishop = Piece(2, "white", (U + R, U + L, D + R, D + L), 3, initial_positions["wb"])
+white_knight = Piece(3, "white", (U + U + R, U + U + L, D + D + R, D + D + L), 3, initial_positions["wn"])
+white_rook = Piece(4, "white", (U, D, R, L), 5, initial_positions["wr"])
+white_queen = Piece(5, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 9, initial_positions["wq"])
+white_king = Piece(6, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000, initial_positions["wk"])
 
-black_pawn = Piece(1, "black", (U, U + U, U + R, U + L ), 1)
-black_bishop = Piece(2, "black", (U + R, U + L, D + R, D + L), 3)
-black_knight = Piece(3, "black", (U + U + R, U + U + L, D + D + R, D + D + L), 3)
-black_rook = Piece(4, "black", (U, D, R, L), 5)
-black_queen = Piece(5, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 9)
-black_king = Piece(6, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000)
+black_pawn = Piece(1, "black", (U, U + U, U + R, U + L ), 1, initial_positions["bp"])
+black_bishop = Piece(2, "black", (U + R, U + L, D + R, D + L), 3, initial_positions["bb"])
+black_knight = Piece(3, "black", (U + U + R, U + U + L, D + D + R, D + D + L), 3, initial_positions["bn"])
+black_rook = Piece(4, "black", (U, D, R, L), 5, initial_positions["br"])
+black_queen = Piece(5, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 9, initial_positions["bq"])
+black_king = Piece(6, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000, initial_positions["bk"])
+
+# uses the piece class to give every piece their own attributes
