51f378c5c063e7a8ef3c7e6753ab2cb6e0d3a11e
mohamed <m_omar01@protonmail.com>
2020-01-30 22:25:14 +0000

    Created a function to check that the user inputs valid moves. Created a class to check that this function works (incomplete).

--- board_rep.py
+++ board_rep.py
@@ -48,7 +48,7 @@ initial_positions = {
     "bn": [92, 97],
     "br": [91, 98],
     "bq": [94],
-    "bk": [95],
+    "bk": [95]
 }
 
 # the positions in the array of the pieces in the initial chess arrangement
@@ -61,12 +61,19 @@ for row in initial_positions.values():
 
 
 class Piece:
+    piece_list = []
+
     def __init__(self, piece_type, colour, moves, value, positions):
         self.piece_type = piece_type
         self.colour = colour
         self.moves = moves
         self.value = value
         self.positions = positions
+
+        Piece.piece_list.append(self)
+
+    def make_move(self, start_pos, end_pos):
+        print(start_pos, end_pos)
 
 
 white_pawn = Piece(1, "white", (U, U + U, U + R, U + L), 1, initial_positions["wp"])
@@ -76,7 +83,7 @@ white_rook = Piece(4, "white", (U, D, R, L), 5, initial_positions["wr"])
 white_queen = Piece(5, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 9, initial_positions["wq"])
 white_king = Piece(6, "white", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000, initial_positions["wk"])
 
-black_pawn = Piece(1, "black", (U, U + U, U + R, U + L ), 1, initial_positions["bp"])
+black_pawn = Piece(1, "black", (U, U + U, U + R, U + L), 1, initial_positions["bp"])
 black_bishop = Piece(2, "black", (U + R, U + L, D + R, D + L), 3, initial_positions["bb"])
 black_knight = Piece(3, "black", (U + U + R, U + U + L, D + D + R, D + D + L), 3, initial_positions["bn"])
 black_rook = Piece(4, "black", (U, D, R, L), 5, initial_positions["br"])
@@ -84,3 +91,64 @@ black_queen = Piece(5, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 9, ini
 black_king = Piece(6, "black", (U, D, R, L, U + R, U + L, D + R, D + L), 100000000, initial_positions["bk"])
 
 # uses the piece class to give every piece their own attributes
+
+
+def get_move():
+    error_msg = "Please enter your move in the correct format, e.g. 'a1b2'."
+
+    while 1:
+        move_string = input("What move would you like to make: ")
+
+        if len(move_string) != 4:
+            print(error_msg)
+
+        else:
+            pos_refs = [move_string[:2], move_string[2:]]  # splits string into the two positions it points to
+            locations = []
+            valid = True
+
+            for part in pos_refs:
+                if (part[0] in letters) and (part[1] in digit_strings):
+                    locations.append(ranks[part[0]] + files[part[1]])  # the array indices of the positions
+                else:
+                    print(error_msg)  # if the string is not in the correct format an error message is displayed
+                    valid = False
+                    break
+
+# def move_choice():
+#     """Takes a string indicating the move to be made and parses it."""
+#
+#     error_msg = "Please enter your move in the correct format, e.g. 'a1b2'."
+#
+#     while 1:
+#         move_string = input("What move would you like to make: ")
+#
+#         if len(move_string) != 4:
+#             print(error_msg)
+#
+#         else:
+#             pos_refs = [move_string[:2], move_string[2:]]  # splits string into the two positions it points to
+#             locations = []
+#             valid = True
+#
+#             for part in pos_refs:
+#                 if (part[0] in letters) and (part[1] in digit_strings):
+#                     locations.append(ranks[part[0]] + files[part[1]])  # the array indices of the positions
+#                 else:
+#                     print(error_msg)  # if the string is not in the correct format an error message is displayed
+#                     valid = False
+#                     break
+#
+#             if valid:
+#                 found = False
+#                 for piece in Piece.piece_list:
+#                     if locations[0] in piece.positions:
+#                         found = True
+#                         piece.make_move(locations[0], locations[1])
+#                         return 0
+#
+#                 if not found:
+#                     print("There is no piece to move at this position.")
+#
+#
+# move_choice()
---
+++ board_rep_test.py
@@ -0,0 +1,6 @@
+import unittest
+from board_rep import *
+
+class TestMoveChoice(unittest.TestCase):
+    def check_move_string(self):
+        """Checks if the function will validate the move string."""
