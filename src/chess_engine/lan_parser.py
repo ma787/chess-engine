bc0ccbf3bd830aca253a63d3cc1d96a42640f920
Mohamed <m_omar01@protonmail.com>
2024-09-16 23:32:01 +0100

    Rewrote convert_lan_to_move function in lan_parser.py. Added small to_index function.

--- src/chess_engine/lan_parser.py
+++ src/chess_engine/lan_parser.py
@@ -1,79 +1,81 @@
+"""This module provides functions that convert valid move strings in long 
+algebraic notation to move objects and vice versa."""
+
 import re
 import string
 
 from chess_engine import attributes as attrs, move, pieces
 
 
+def to_index(square):
+    """Converts a string coordinate to a board array index."""
+    return (int(square[1]) - 1, string.ascii_lowercase.index(square[0]))
+
+
 def convert_lan_to_move(move_string, board):
-    """Validates and changes the move string entered by the user to a move class."""
-    piece_check = re.fullmatch("[BKNQR][a-h][1-8][x-][a-h][1-8]", move_string)
-    pawn_check = re.fullmatch("[a-h][1-8][x-][a-h][1-8][BKNQR]?", move_string)
-    castling_check = move_string in ("0-0", "0-0-0")
+    """Validates and changes the move string entered by the user to a move object.
+
+    Args:
+        move_string (string): The LAN representation of the requested move.
+        colour (Colour): The current side to move on the board. Used to
+        determine castle move coordinates.
+
+    Returns:
+        Move: The move object represented by the move string.
+    """
+    piece_types = {
+        "B": pieces.Bishop,
+        "K": pieces.King,
+        "N": pieces.Knight,
+        "Q": pieces.Queen,
+        "R": pieces.Rook,
+    }
 
     promotion = None
     castling = None
     capture = False
 
-    if piece_check:
+    if re.fullmatch("[BKNQR][a-h][1-8][x-][a-h][1-8]", move_string) is not None:
+        p_type = piece_types[move_string[0]]
         capture = move_string[3] == "x"
-        start_string = move_string[1:3]
-        end_string = move_string[-2:]
 
-    elif pawn_check:
-        for p in (pieces.Bishop, pieces.Knight, pieces.Rook, pieces.Queen, pieces.King):
-            if p.symbol == move_string[-1].lower():
-                promotion = p
+        start_coord = to_index(move_string[1:3])
+        end_coord = to_index(move_string[-2:])
+
+    elif re.fullmatch("[a-h][1-8][x-][a-h][1-8][BKNQR]?", move_string) is not None:
+        for p, t in piece_types.items():
+            if move_string[-1] == p:
+                promotion = t
                 break
 
+        p_type = pieces.Pawn
         capture = move_string[2] == "x"
-        start_string = move_string[0:2]
 
-        if promotion:
-            end_string = move_string[-3:-1]
-        else:
-            end_string = move_string[-2:]
+        start_coord = to_index(move_string[0:2])
+        end_coord = (
+            to_index(move_string[-3:-1])
+            if promotion is not None
+            else to_index(move_string[-2:])
+        )
 
-    elif castling_check:
-        split_string = move_string.split("-")
+    elif move_string in ("0-0", "0-0-0"):
+        p_type = pieces.King
+        castling = (
+            attrs.Castling.QUEEN_SIDE
+            if move_string == "0-0-0"
+            else attrs.Castling.KING_SIDE
+        )
+        file = 2 if castling == attrs.Castling.QUEEN_SIDE else 4
 
-        if len(split_string) == 3:
-            castling = attrs.Castling.QUEEN_SIDE
-            end_letter = "c"
-        else:
-            castling = attrs.Castling.KING_SIDE
-            end_letter = "g"
-
-        if board.side_to_move == attrs.Colour.WHITE:
-            start_string = "e1"
-            end_string = end_letter + "1"
-
-        else:
-            start_string = "e8"
-            end_string = end_letter + "8"
+        start_coord = (7 - board.final_rank, 4)
+        end_coord = (7 - board.final_rank, file)
     else:
-        return None
-
-    start_coord = (
-        int(start_string[1]) - 1,
-        string.ascii_lowercase.index(start_string[0]),
-    )
-    end_coord = (int(end_string[1]) - 1, string.ascii_lowercase.index(end_string[0]))
-
-    piece = board.array[start_coord[0]][start_coord[1]]
-
-    if not piece:
-        return None
-
-    if pawn_check and piece.symbol != "p":
-        return None
-
-    if piece_check and piece.symbol != move_string[0].lower():
         return None
 
     return move.Move(
         start_coord,
         end_coord,
-        type(piece),
+        piece_type=p_type,
         capture=capture,
         castling=castling,
         promotion=promotion,
