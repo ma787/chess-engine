3f51f4b94a07dbe388730d53a2f9abe2db8c3cc5
Mohamed <m_omar01@protonmail.com>
2024-09-17 01:31:05 +0100

    Added module and class docstrings to engine. Modified alpha_beta_search function.

--- src/chess_engine/engine.py
+++ src/chess_engine/engine.py
@@ -1,18 +1,30 @@
+"Module providing the chess engine implementation."
+
 import math
 
 from chess_engine import (
     attributes as attrs,
     hashing,
-    lan_parser as lan,
+    lan_parser as lp,
     move_generation as mg,
 )
 
 
 class Engine:
+    """The chess engine implementation.
+
+    Attributes:
+        colour (Colour): The side to move of the engine object.
+        hashing (Hashing)": The Hashing object to use for hashing board positions.
+        t_table (dict): The transposition table, used to store previously
+            evaluated positions and their scores:
+                hashed position: (move_string, score)
+    """
+
     def __init__(self, colour):
         self.colour = colour
         self.hashing = hashing.Hashing()
-        self.t_table = {}  # hashed board position: (move string, score)
+        self.t_table = {}
 
     def alpha_beta_search(self, board, alpha, beta, depth):
         """Finds the highest score attainable from the current position."""
@@ -21,30 +33,43 @@ class Engine:
 
         board_hash = self.hashing.zobrist_hash(board)
 
-        if board_hash in self.t_table.keys():
-            return self.t_table[board_hash][1]
+        for key, _ in self.t_table.items():
+            if board_hash == key:
+                return self.t_table[board_hash][1]
 
         value = -math.inf
+
         moves = mg.all_possible_moves(board)
+        no_moves = len(moves) == 0
         best_move = None
 
-        for move in moves:
-            move.make_move(board)
-            value = max(value, -self.alpha_beta_search(board, -beta, -alpha, depth - 1))
+        if no_moves:
+            if not mg.in_check(board):
+                value = 0
 
-            if value >= beta:
+        else:
+            for move in moves:
+                move.make_move(board)
+                value = max(
+                    value, -self.alpha_beta_search(board, -beta, -alpha, depth - 1)
+                )
+
+                if value >= beta:
+                    move.unmake_move(board)
+                    self.t_table[board_hash] = (lp.convert_move_to_lan(move), value)
+                    return beta  # fail-high node
+
+                if value > alpha:
+                    best_move = move
+                    alpha = value
+
                 move.unmake_move(board)
-                self.t_table[board_hash] = (lan.convert_move_to_lan(move), value)
-                return beta  # beta cutoff
 
-            if value > alpha:
-                best_move = move
-                alpha = value
+        if best_move is not None:
+            best_move = lp.convert_move_to_lan(best_move)
 
-            move.unmake_move(board)
-
-        if best_move:
-            best_move = lan.convert_move_to_lan(best_move)
+        else:  # either a terminal or a fail-low node
+            best_move = None if no_moves else moves[0]
 
         self.t_table[board_hash] = (best_move, value)
         return alpha
